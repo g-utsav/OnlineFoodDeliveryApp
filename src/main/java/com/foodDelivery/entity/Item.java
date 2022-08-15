6415b6d3fcc5d95ea57197367b2d3b12c7f3d6c1
Shubham Pawar <shubhampawar09011998@gmail.com>
2022-08-15 17:52:49 +0530

    Item and Category entities updated

--- src/main/java/com/foodDelivery/entity/Item.java
+++ src/main/java/com/foodDelivery/entity/Item.java
@@ -28,11 +28,9 @@ public class Item {
 	@NotNull
 	private double cost;
 	
-	//@Autowired
 	@OneToOne(cascade = CascadeType.ALL)
 	private Category category;
 	
-	//@Autowired
 	@ManyToMany(cascade = CascadeType.ALL,mappedBy = "itemList")
 	List<Restaurant> restaurantList=new ArrayList<>();
 
@@ -70,7 +68,6 @@ public class Item {
 
 	public Item() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public List<Restaurant> getRestaurantList() {
